46ba22e0afe99961b2a4fe28334abd71d7fa5785
Yaroslav <yar21sav@gmail.com>
2022-09-11 17:56:51 +0300

    add enjoye script, change structure od reach env

---
+++ py_scripts/enjoy.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+from time import sleep
+import argparse
+import numpy as np
+import pybullet
+from stable_baselines3 import PPO
+import os
+import shutil
+screencast = False
+headless = False
+# Instantiate the parser
+parser = argparse.ArgumentParser(description='Enjoyer optional arguments')
+
+parser.add_argument('--headless', 
+                    action='store_true',
+                    help="do not use bullet GUI", 
+                    default=False)
+parser.add_argument('--screencast', 
+                    action='store_true',
+                    help="capture video from bullet GUI", 
+                    default=False)
+
+parser.add_argument('-m', '--model', 
+                    type=str, 
+                    help="Path to trained model (default: ./trained_models/[SKILL]/UR_ENV/best_model.zip)", 
+                    default=None)
+
+parser.add_argument('-a', '--algo',
+                    type=str, 
+                    default='PPO',
+                    help='[PPO, SAC, TD3] (default: PPO)')
+
+parser.add_argument('-s', '--skill', 
+                    type=str, 
+                    default='reach',
+                    help='[reach, reach_obj, push, slide, pick] (default: reach)')          
+
+def screencast(func):
+    def wrapper(episode_id, headless):
+        if screencast and not headless:
+            if episode_id == 0:
+                try:
+                    shutil.rmtree(f"./screencast/{args.skill}")
+                except OSError as e:
+                    pass
+                os.mkdir(f"./screencast/{args.skill}")
+            filename = f"./screencast/{args.skill}/{args.algo}_{str(episode_id)}.mp4"
+            print(f'logging into "{filename}"')
+            pybullet.startStateLogging(pybullet.STATE_LOGGING_VIDEO_MP4, filename)
+            out = func(episode_id, headless)
+            pybullet.stopStateLogging(pybullet.STATE_LOGGING_VIDEO_MP4)
+        else:
+            out = func(episode_id, headless)
+        return out
+    return wrapper
+
+@screencast
+def play_episode(episode_id, headless = False):
+    obs = enjoy_env.reset()
+    done = False
+    episode_reward = 0.0
+    episode_length = 0
+    step = 0
+    is_success = False
+    while not done:
+        action, _ = model.predict(obs, deterministic=True)
+        obs, reward, done, info = enjoy_env.step(action)
+        is_success = info['is_success'] # get latest success status
+        episode_reward += reward
+        episode_length += 1
+        step += 1
+        dt = 1.0 / 120.0
+        if not headless:
+            sleep(dt)
+    return episode_reward, episode_length, is_success
+
+if __name__ == "__main__":
+    
+    args = parser.parse_args()
+    if args.model == None:
+        args.model = "trained_models/UR_ENV_" + args.skill + "/best_model.zip"
+    save_path = args.model
+    print("-----------------------------------------\n")
+    print("skill   :", args.skill)
+    print("algo    :", args.algo)
+    print("viz     :", not args.headless)
+    print("model   :", args.model)
+    print("scrcast :", args.screencast)
+    print("\n-----------------------------------------")
+    
+    RL_ALGORITHM = args.algo
+    N_ITERATIONS = 1000
+    if args is not None:
+        headless = args.headless
+        screencast = args.screencast
+    #choose environment for skill 
+    try:
+        if args.skill == 'reach': 
+            from envs.reach_env import Env as ENV   
+        else:
+            raise ImportError
+    except ImportError as error:
+        print('ERROR: Requested skill "{0}" not implemented yet.'.format(args.skill))
+        exit()
+
+    algo = {
+        "PPO": PPO,
+    }[RL_ALGORITHM]
+    enjoy_env = ENV(headless)
+    
+    try:
+        model = algo.load(save_path, env=ENV)
+    except FileNotFoundError:
+        print("-----------------------------------------\n")
+        print('ERROR: File "{0}" does not exist.'.format(save_path))
+        print("\n-----------------------------------------")
+        exit()
+
+    try:
+        # Use deterministic actions for evaluation
+        episode_rewards, episode_lengths, episode_success = [], [], []
+        for episode_id in range(N_ITERATIONS):
+            episode_reward, episode_length, is_success = play_episode(episode_id, headless)
+            episode_rewards.append(episode_reward)
+            episode_lengths.append(episode_length)
+            episode_success.append(is_success)
+            print(
+                f"Episode {len(episode_rewards)} reward={episode_reward}, length={episode_length}, is success={str(is_success)}"
+            )
+
+           
+
+        
+    except KeyboardInterrupt:
+        pass
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+    mean_success, std_success = np.mean(episode_success), np.std(episode_success)
+
+    mean_len, std_len = np.mean(episode_lengths), np.std(episode_lengths)
+    print("\n-------------------------")
+    print("    ==== Results ====")
+    print(f"Episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
+    print(f"Episode_length={mean_len:.2f} +/- {std_len:.2f}")
+    print(f"Episode_success={mean_success:.2f} +/- {std_success:.2f}")
+    print("-------------------------")
+        
+    # Close process
+    enjoy_env.close()
--- py_scripts/envs/reach_env.py
+++ py_scripts/envs/reach_env.py
@@ -1,3 +1,7 @@
+'''
+Gym environment for the reach skill of the robot arm.
+'''
+
 import gym
 import numpy as np
 from envs.ur5_sim import UR5
@@ -11,9 +15,21 @@ class Env(UR5, gym.Env):
     reward_range = (-np.inf, 0)
 
     def __init__(self, headless=False):
+        '''
+        @param headless: if True, the simulation is run in headless mode
+        '''
         UR5.__init__(self, headless)
     
     def step(self, action = None):
+        '''
+        Perform a step in the environment.
+        @param action: output of the agent
+        @return: namedtuple with the following fields:
+            state: current state of the environment
+            reward: reward for the current step
+            is_done: True if the episode is done
+            info: additional information
+        '''
         self._step_id += 1
         if not action is None:
             action = self._compute_action(action)
@@ -24,42 +40,85 @@ class Env(UR5, gym.Env):
         return output.state, output.reward, output.is_done, output.info
 
     def _compute_action(self, agent_input):
+        '''
+        Convert the agent input to the action.
+        @param agent_input: agent input
+        @return: action
+        '''
         return agent_input
        
     def _compute_output(self):
+        '''
+        Compute the output of the environment for the agent.
+        Info contains the following fields:
+            is_success: True if the episode is successful
+            observation: observation of the environment
+            cumulative_reward: cumulative reward of the episode
+        @return: namedtuple with the following fields:
+            state: current state of the environment
+            reward: reward for the current step
+            is_done: True if the episode is done
+            info: additional information
+        '''
         output = namedtuple('output', ["state", "reward", "is_done", "info"])
         observation = self._observation()
         output.state = self._compute_state(observation)
         output.reward = self._compute_reward(observation)
-        output.is_done = self._is_done(observation)
-        if output.is_done:
-            self._is_success = True
+        is_success = self._is_success(observation)
+        output.is_done = self._is_done(observation, is_success)
         output.info = {
-            "is_done": output.is_done,
-            "is_success": self._is_success,
-            "reward": output.reward,
+            "is_success": is_success,
+            "cumulutive_reward": self._cumulative_reward,
             "observation": observation
         }
         return output
 
     def _compute_state(self, observation):
+        '''
+        Compute the state.
+        State: [gripper_pos, obj_pos]
+        '''
         return {"observation": np.concatenate((observation.gripper_pos, observation.obj_pos))}
 
     def _compute_reward(self, observation):
-        return -np.linalg.norm(observation.gripper_pos - observation.obj_pos)
+        '''
+        Compute the reward.
+        Reward: -dst(gripper, object)
+        @param observation: observation of the environment
+        @return: reward for the current step
+        '''
+        rew = -np.linalg.norm(observation.gripper_pos - observation.obj_pos)
+        self._cumulative_reward += rew
+        return rew
 
-    def _is_done(self, observation):
+    def _is_done(self, observation, is_success):
+        '''
+        Check if the episode is done.
+        Conditions: more than 500 steps for current episode or success is achieved
+        '''
+        return self._step_id == 500 or is_success
+
+    def _is_success(self, observation):
+        '''
+        Check if the episode is successful.
+        Conditions: distance between gripper and object is less than 0.05 for 50 consecutive steps
+        @param observation: observation of the environment
+        @return: True if the episode is successful
+        '''
         if np.linalg.norm(observation.gripper_pos - observation.obj_pos) < 0.05:
             self._steps_at_target += 1
         else:
             self._steps_at_target = 0
-        return self._step_id == 500 or self._steps_at_target >= 50 
+        return self._steps_at_target >= 50 
 
     def reset(self):
+        '''
+        Reset the environment.
+        @return: initial state of the environment
+        '''
         self._reset_world()
         self._steps_at_target = 0
         self._step_id = 0
-        self._reward = 0
-        self._is_success = False
+        self._cumulative_reward = 0
         state = self._compute_state(self._observation())
         return state
